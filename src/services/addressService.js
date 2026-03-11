78e4765f0483010e47d50dfaaca376315ebc3a61
shashank chourasia01 <shashankchourasia0829@gmail.com>
2026-03-11 14:34:59 +0530

    update checkout page

--- src/services/addressService.js
+++ src/services/addressService.js
@@ -18,3 +18,27 @@ const addressService = {
 };
 
 export default addressService;
+
+
+
+
+// import api from './api';
+
+// const addressService = {
+//   // Get user addresses
+//   getAddresses: () => api.get('/users/addresses'),
+  
+//   // Add new address
+//   addAddress: (addressData) => api.post('/users/addresses', addressData),
+  
+//   // Update address
+//   updateAddress: (id, addressData) => api.put(`/users/addresses/${id}`, addressData),
+  
+//   // Delete address
+//   deleteAddress: (id) => api.delete(`/users/addresses/${id}`),
+  
+//   // Set default address
+//   setDefaultAddress: (id) => api.put(`/users/addresses/${id}/default`)
+// };
+
+// export default addressService;
